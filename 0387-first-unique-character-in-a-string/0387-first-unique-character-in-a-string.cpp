53234156d8e94bb59038e9541d91a88bb4fd7ae0
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2025-12-21 18:45:40 +0530

    Time: 21 ms (19.43%), Space: 15 MB (20.31%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> freq;
+
+        // count frequency
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        // find first unique
+        for (int i = 0; i < s.size(); i++) {
+            if (freq[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
