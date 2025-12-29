0435f038017d33bd4ab57a0db6fef3b3fded56b6
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2025-12-29 19:46:24 +0530

    Time: 0 ms (100%), Space: 19.5 MB (30.55%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> mp;   // prefix hashmap: value → index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            // prefix check (have we seen 'need' before?)
+            if (mp.count(need)) {
+                return { mp[need]+1, i+1 };
+            }
+
+            // store current value as prefix
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
