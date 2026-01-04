f185983108e59e8eae620980056d15b05662bc16
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-01-04 17:31:24 +0530

    Time: 0 ms (100%), Space: 8.3 MB (82.74%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            // process string s
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            // process string t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // compare current characters
+            if (i >= 0 && j >= 0) {
+                if (s[i] != t[j]) return false;
+            } else {
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+};
