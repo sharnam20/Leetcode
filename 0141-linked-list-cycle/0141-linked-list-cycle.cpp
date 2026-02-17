60987f11280c607fc829de1f98202fddfa26fee9
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-02-17 05:36:03 +0530

    Time: 12 ms (42.71%), Space: 11.8 MB (80.79%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -3,18 +3,36 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      //  approach 1
-        unordered_set<ListNode*> visited;
+    //   //  approach 1
+    //     unordered_set<ListNode*> visited;
 
-        while (head != NULL) {
-            if (visited.find(head) != visited.end()) {
-                return true;   // cycle detected
+    //     while (head != NULL) {
+    //         if (visited.find(head) != visited.end()) {
+    //             return true;   // cycle detected
+    //         }
+
+    //         visited.insert(head);
+    //         head = head->next;
+    //     }
+
+    //     return false;  // no cycle
+
+    // slow and fast pointer
+     if (head == NULL || head->next == NULL)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;          
+            fast = fast->next->next;    
+
+            if (slow == fast) {
+                return true;   
             }
-
-            visited.insert(head);
-            head = head->next;
         }
 
-        return false;  // no cycle
+        return false;  
     }
 };
