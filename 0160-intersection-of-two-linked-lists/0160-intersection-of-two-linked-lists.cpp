29c93726e12fd9068a666862af954dfddadb6257
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-02-21 05:20:34 +0530

    Time: 61 ms (34.45%), Space: 24.1 MB (24.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -33,21 +33,41 @@ public:
 
 //    ---------------- bruteforce 2
       // we use set
-      unordered_set<ListNode*>s;
-      ListNode* a = headA;
-      while(a!= NULL){
-        s.insert(a);
-         a = a->next;
-      }
-      ListNode* b = headB;
-      while(b!=NULL){
-        if(s.find(b)!=s.end()){
-            return b;
-        }
-         b = b->next;
+    //   unordered_set<ListNode*>s;
+    //   ListNode* a = headA;
+    //   while(a!= NULL){
+    //     s.insert(a);
+    //      a = a->next;
+    //   }
+    //   ListNode* b = headB;
+    //   while(b!=NULL){
+    //     if(s.find(b)!=s.end()){
+    //         return b;
+    //     }
+    //      b = b->next;
        
+    //   }
+    //   return NULL;
+    // optimal 1 using 2 pointer 
+      ListNode* a = headA;
+      ListNode* b = headB;
+     if(headA == NULL || headB == NULL)
+            return NULL;
+      while(a!=b){
+        if(a==NULL){
+            a=headB;
+        }
+        else{
+            a=a->next;
+        }
+        if(b==NULL){
+            b=headA;
+        }
+        else{
+            b=b->next;
+        }
       }
-      return NULL;
+      return a;
     }
 };
   
