8c9c314626c5ec4c94d8427ac6f105c309596a2a
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-03-02 14:34:18 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.74%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -20,21 +20,62 @@ public:
         if (head==NULL){
             return NULL;
         }
-        Node*temp=head;
-        unordered_map<Node*,Node*>mp;
-        while(temp!=NULL){
-            Node*newnode=new Node(temp->val);//create copy node value same of original
-            mp[temp]=newnode;
-            temp=temp->next;
-        }
-        // after this while loop in map original->nwenode with value ;
-        temp=head;
-        while(temp!=NULL){
-            mp[temp]->next=mp[temp->next];
-             mp[temp]->random=mp[temp->random];
-             temp=temp->next;
+        //approach -1 --------------------------------
+        // Node*temp=head;
+        // unordered_map<Node*,Node*>mp;
+        // while(temp!=NULL){
+        //     Node*newnode=new Node(temp->val);//create copy node value same of original
+        //     mp[temp]=newnode;
+        //     temp=temp->next;
+        // }
+        // // after this while loop in map original->nwenode with value ;
+        // temp=head;
+        // while(temp!=NULL){
+        //     mp[temp]->next=mp[temp->next];
+        //      mp[temp]->random=mp[temp->random];
+        //      temp=temp->next;
 
+        // }
+        // return mp[head];
+        //approach -2-----------------------------
+ 
+    
+        
+        Node* temp = head;
+        
+        // Step 1: Insert copy nodes after original nodes
+        while(temp != NULL) {
+            Node* newNode = new Node(temp->val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = newNode->next;
         }
-        return mp[head];
+        
+        // Step 2: Assign random pointers
+        temp = head;
+        while(temp != NULL) {
+            if(temp->random != NULL)
+                temp->next->random = temp->random->next;
+            
+            temp = temp->next->next;
+        }
+        
+        // Step 3: Separate original and copied list
+        Node* original = head;
+        Node* copyHead = head->next;
+        Node* copy = copyHead;
+        
+        while(original != NULL) {
+            original->next = original->next->next;
+            
+            if(copy->next != NULL)
+                copy->next = copy->next->next;
+            
+            original = original->next;
+            copy = copy->next;
+        }
+        
+        return copyHead;
     }
 };
+ 
