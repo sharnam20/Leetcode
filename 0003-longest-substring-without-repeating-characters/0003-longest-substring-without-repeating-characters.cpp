44a0278c57f0c12f18823e5760212c8249d58d6d
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-03-30 22:36:28 +0530

    Time: 20 ms (23.1%), Space: 12.3 MB (41.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,24 +6,22 @@ public:
         int maxi=0;
         unordered_map<char,int>mp;
         while(j<s.length()){
-             mp[s[j]]++;
+            mp[s[j]]++;
             if(mp[s[j]]==1){
-               
                 maxi=max(maxi,j-i+1);
                 j++;
-                }
+            }
             else if(mp[s[j]]>1){
-               while(mp[s[j]]>1){
+                while(mp[s[j]]>1){
                 mp[s[i]]--;
-                 if(mp[s[i]]==0){
-                       mp.erase(s[i]);
-                     
-                    }
-                    i++;
-               }
-               j++;
+               if(mp[s[i]]==0){
+               mp.erase(s[i]);}
+
+                i++;
+                }
+                j++;
+            }
         }
-        }
-      return maxi;  
+        return maxi;
     }
 };
