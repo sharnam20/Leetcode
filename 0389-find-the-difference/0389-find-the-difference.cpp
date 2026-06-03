160b2971867ed3c4d70436a3b8671edac05d1c25
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-06-03 18:02:42 +0530

    Time: 0 ms (100%), Space: 9.4 MB (24.95%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            ans ^= s[i];
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            ans ^= t[i];
+        }
+
+        return ans;
+    }
+};
