b2bf00dd2ea23097e8adbeffad513d2fb69ece08
sharnam20 <166909101+sharnam20@users.noreply.github.com>
2026-07-11 18:13:09 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int search(std::vector<int>& nums, int target) { 
-        int n = nums.size();
-
-        // Step 2: Binary search for target
-
-        int left = 0, right = n - 1, mid;
-
-        while (left <= right) {
-            // check sort left or right
-            mid = (left + right) / 2;
-            if (nums[mid] == target)
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
                 return mid;
-           if(nums[left]<=nums[mid]){ 
-            if(nums[left]<=target && target<=nums[mid]){
-                  right=mid-1;
-            }else{
-                left=mid+1;
             }
-           }
-           else{
-            if(nums[mid]<=target && target<=nums[right]){
-                left=mid+1;
-            }else{
-                right=mid-1;
+            else if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
-           }
+              else if(nums[mid]<=nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+
         }
         return -1;
     }
-
-  
 };
